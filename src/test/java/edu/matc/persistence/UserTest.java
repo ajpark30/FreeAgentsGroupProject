d404b28747a1fbd9c6b4f4b0e8e9f3fcc050a81e
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-12 21:02:49 -0500

    Add stubbed test classes for entitys and endpoints

---
+++ src/test/java/edu/matc/persistence/UserTest.java
@@ -0,0 +1,29 @@
+package edu.matc.persistence;
+
+public class UserTest {
+    public void setUp() {
+    }
+
+    public void testGetByPropertyEqual() {
+    }
+
+//   These tests will be used if the DAO
+//   has methods for them
+//    public void testGetByPropertyLike() {
+//    }
+//
+//    public void testGetByProperties() {
+//    }
+
+    public void testGetAll() {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testInsert() {
+    }
+
+    public void testDelete() {
+    }
+}
---
+++ src/test/java/edu/matc/persistence/WaterfallTest.java
@@ -0,0 +1,31 @@
+package edu.matc.persistence;
+
+public class WaterfallTest {
+
+    public void setUp() {
+    }
+
+    public void testGetByPropertyEqual() {
+    }
+
+//   These tests will be used if the DAO
+//   has methods for them
+//    public void testGetByPropertyLike() {
+//    }
+//
+//    public void testGetByProperties() {
+//    }
+
+    public void testUpdate() {
+    }
+
+    public void testGetAll() {
+    }
+
+    public void testInsert() {
+    }
+
+    public void testDelete() {
+    }
+
+}
