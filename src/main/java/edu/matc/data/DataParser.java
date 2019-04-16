6ca6ac6cb6cd0bcbb0f50328056423491d041408
Jacob Bong <jacobpricebong@gmail.com>
2019-04-15 20:31:50 -0500

    Commiting jsoup links

--- src/main/java/edu/matc/data/DataParser.java
+++ src/main/java/edu/matc/data/DataParser.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Selector;
 
 public class DataParser {
     public static void main(String args[]) {
+
+        Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/List_of_waterfalls").get();
+        /*
         String DataToParse = "<!DOCTYPE html>"
                 + "<html>"
                 + "<head>"
@@ -22,5 +26,8 @@ public class DataParser {
         Document html = Jsoup.parse(DataToParse);
         String title = html.title;
         String name = html.body().getElementsByTag("h1").text();
+        String location = html.body().getElementsByTag("p").text();
+        */
+        Elements waterfallLinks = doc.select("a[href^=`/wiki`]");
     }
 }
