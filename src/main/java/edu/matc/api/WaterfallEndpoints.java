635488ce1155df7d4c1e139a78d7730694140539
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-18 14:37:21 -0500

    Add work in progress delete method

--- src/main/java/edu/matc/api/WaterfallEndpoints.java
+++ src/main/java/edu/matc/api/WaterfallEndpoints.java
@@ -2,10 +2,13 @@ package edu.matc.api;
 
 import edu.matc.persistence.GenericDao;
 import edu.matc.entity.Waterfall;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/waterfall")
 public class WaterfallEndpoints {
@@ -14,9 +17,22 @@ public class WaterfallEndpoints {
 
     @GET
     public Response getWaterfall(@QueryParam("id") String id) {
-        Waterfall waterfall = DAO.getByPropertyEqual("waterfall_id", "1").get(0);
+        List<Waterfall> waterfalls = DAO.getByPropertyEqual("waterfall_id", id);
 
-        return Response.status(200).entity(waterfall.getName()).build();
+        if (waterfalls.size() == 0) {
+            return Response.status(404).entity("Waterfall not found").build();
+        }
+
+        return Response.status(200).entity(waterfalls.get(0).getName()).build();
+    }
+
+    @DELETE
+    public Response deleteWaterfall(@QueryParam("id") String id) {
+        List<Waterfall> waterfalls = DAO.getByPropertyEqual("waterfall_id", id);
+
+//        if (waterfalls.size()) {
+//        }
+        return Response.status(200).entity("...").build();
     }
 
 }
