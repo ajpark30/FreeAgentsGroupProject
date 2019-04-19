1e2fa81d8af358fac2e4b5e4a203b3606fc9ef01
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-18 19:20:08 -0500

    Add insert and update test, use new constructor
    Added an insert and update test, changed the mock object in
    testGetByPropertyEqual to be constructed with the lat + long + name
    constructor.

--- src/test/java/edu/matc/persistence/WaterfallTest.java
+++ src/test/java/edu/matc/persistence/WaterfallTest.java
@@ -30,11 +30,8 @@ public class WaterfallTest {
 
     @Test
     public void testGetByPropertyEqual() {
-        Waterfall mockWaterfall = new Waterfall();
+        Waterfall mockWaterfall = new Waterfall("Kalandula Falls", -9.07583f, 16.0033f);
         mockWaterfall.setWaterfallId(1);
-        mockWaterfall.setName("Kalandula Falls");
-        mockWaterfall.setLatitude(-9.07583f);
-        mockWaterfall.setLongitude(16.0033f);
 
         List<Waterfall> foundWaterfalls = dao.getByPropertyEqual("waterfall_id", "1");
 
@@ -42,25 +39,42 @@ public class WaterfallTest {
         assertEquals(mockWaterfall.toString(), foundWaterfalls.get(0).toString());
     }
 
-//   These tests will be used if the DAO
-//   has methods for them
-//    public void testGetByPropertyLike() {
-//    }
-//
-//    public void testGetByProperties() {
-//    }
 
+    public void testGetByPropertyLike() {
+    }
+
+    public void testGetByProperties() {
+    }
+
+    @Test
     public void testUpdate() {
+        Waterfall waterfall = dao.getByPropertyEqual("waterfall_id", "1").get(0);
+        waterfall.setLongitude(333.33f);
+
+        dao.saveOrUpdate(waterfall);
+
+        List<Waterfall> foundWaterfalls = dao.getByPropertyEqual("waterfall_id", "1");
+
+        assertEquals(1, foundWaterfalls.size());
+        assertEquals(333.33f, foundWaterfalls.get(0).getLongitude());
     }
 
     @Test
     public void testGetAll() {
         List<Waterfall> waterfalls = dao.getAll();
-
         assertEquals(4, waterfalls.size());
     }
 
+    @Test
     public void testInsert() {
+        Waterfall waterfallToAdd = new Waterfall("Foo Falls", -10.002f, 34.394f);
+
+        int id = dao.insert(waterfallToAdd);
+
+        List<Waterfall> foundWaterfalls = dao.getByPropertyEqual("waterfall_id", String.valueOf(id));
+
+        assertEquals(1, foundWaterfalls.size());
+        assertEquals(waterfallToAdd.toString(), foundWaterfalls.get(0).toString());
     }
 
     public void testDelete() {
