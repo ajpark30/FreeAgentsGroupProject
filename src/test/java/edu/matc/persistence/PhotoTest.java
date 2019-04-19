d2535db7a93b3ad92d835115f6399381933a8373
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-19 00:35:41 -0500

    Add getbyProperty Equal and Like test methods

--- src/test/java/edu/matc/persistence/PhotoTest.java
+++ src/test/java/edu/matc/persistence/PhotoTest.java
@@ -1,17 +1,52 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.Photo;
+import edu.matc.entity.Waterfall;
+import edu.matc.util.DatabaseUtility;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhotoTest {
 
+    private GenericDao<Photo> dao;
+
+    @BeforeAll
+    @AfterAll
+    public static void resetDatabase() {
+        DatabaseUtility dbUtil = new DatabaseUtility();
+        dbUtil.runSQL("target/test-classes/create_waterfalls_db.sql");
+    }
+
+    @BeforeEach
     public void setUp() {
+        dao = new GenericDao<>(Photo.class);
+        DatabaseUtility dbUtil = new DatabaseUtility();
+        dbUtil.runSQL("target/test-classes/photoSetup.sql");
+    }
+
+    @Test
+    public void testGetByPropertyEqual() {
+        Photo mockPhoto = getMockPhoto();
+
+        List<Photo> foundPhotos = dao.getByPropertyEqual("photo_id", "1");
+
+        assertEquals(1, foundPhotos.size());
+        assertEquals(mockPhoto.toString(), foundPhotos.get(0).toString());
     }
 
     public void testGetById() {
     }
 
-    public void testGetByPropertyEqual() {
-    }
-
+    @Test
     public void testGetByPropertyLike() {
+        List<Photo> foundPhotos = dao.getByPropertyLike("sourceURL", "wikipedia");
+        assertEquals(2, foundPhotos.size());
     }
 
     public void testGetAll() {
@@ -25,4 +60,16 @@ public class PhotoTest {
 
     public void testDelete() {
     }
+
+    private Photo getMockPhoto() {
+        GenericDao<Waterfall> waterfallDao = new GenericDao<>(Waterfall.class);
+
+        Photo mockPhoto = new Photo();
+        mockPhoto.setPhotoId(1);
+        mockPhoto.setWaterfall(waterfallDao.getById(3));
+        mockPhoto.setSourceURL("https://upload.wikimedia.org/wikipedia/commons/3/3a/GrandfatherExposedPreCambrian.jpg");
+
+        return mockPhoto;
+    }
+
 }
