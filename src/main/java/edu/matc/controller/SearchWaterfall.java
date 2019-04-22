32e4b273957311effe56e94278430f075a524a78
ajpark30 <apark@madisoncollege.edu>
2019-04-22 13:29:14 -0500

    Adding work that was basically dead.  The important thing I added was the update to Index which will describe to the user how to use our REST service.

--- src/main/java/edu/matc/controller/SearchWaterfall.java
+++ src/main/java/edu/matc/controller/SearchWaterfall.java
@@ -1,6 +1,7 @@
 package edu.matc.controller;
 
 import edu.matc.entity.Coordinates;
+import edu.matc.entity.Photo;
 import edu.matc.entity.Waterfall;
 import edu.matc.persistence.GenericDao;
 import edu.matc.persistence.WaterfallDao;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Add docs here
@@ -34,21 +36,34 @@ public class SearchWaterfall extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         WaterfallDao waterfallDao = new WaterfallDao();
+        GenericDao<Photo> genericDaoPhoto = new GenericDao<>(Photo.class);
+        //Photo photo = new Photo();
 
         if (req.getParameter("submit").equals("searchByZip")) {
             Coordinates coordinates = waterfallDao.coordsFromZipcode(req.getParameter("searchZip"));
-            req.setAttribute("waterFallInfo", waterfallDao.findWaterfallsNear(coordinates));
+            List<Waterfall> waterfall = waterfallDao.findWaterfallsNear(coordinates);
+            req.setAttribute("waterFallInfo", waterfall);
+            //req.setAttribute("photo", genericDaoPhoto.getById(waterfall.get(0).getWaterfallId()));
+            logger.info("***Photo information for UX: " + genericDaoPhoto.getAll());
+            logger.info("^^^Waterfall get all: " + waterfall.get(0).getWaterfallId());
+            logger.info("$$$Photo by ID: " + genericDaoPhoto.getById(waterfall.get(0).getWaterfallId()));
         }
         if (req.getParameter("submit").equals("searchByName")) {
-            req.setAttribute("waterFallInfo", waterfallDao.getByPropertyEqual("name", req.getParameter("searchName")));
+            List<Waterfall> waterfall = waterfallDao.getByPropertyEqual("name", req.getParameter("searchName"));
+            req.setAttribute("waterFallInfo", waterfall);
+            logger.info("***Photo information for UX: " + genericDaoPhoto.getById(1));
         }
         if (req.getParameter("submit").equals("searchByLatLong")) {
             String latitude = req.getParameter("searchLatitude");
             String longitude = req.getParameter("searchLongitude");
-            req.setAttribute("waterFallInfo", waterfallDao.findNearest(Double.parseDouble(latitude), Double.parseDouble(longitude)));
+            List<Waterfall> waterfall = waterfallDao.findNearest(Double.parseDouble(latitude), Double.parseDouble(longitude));
+            req.setAttribute("waterFallInfo", waterfall);
+            logger.info("***Photo information for UX: " + genericDaoPhoto.getById(3));
+            logger.info("^^^Waterfall get all: " + waterfall.get(0).getName());
         }
         if (req.getParameter("submit").equals("searchAll")) {
-            req.setAttribute("waterFallInfo", waterfallDao.getAll());
+            List<Waterfall> waterfall = waterfallDao.getAll();
+            req.setAttribute("waterFallInfo", waterfall);
         }
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/waterfallResults.jsp");
--- src/main/webapp/waterfallResults.jsp
+++ src/main/webapp/waterfallResults.jsp
@@ -7,17 +7,17 @@
 <div class="container-fluid">
     <h2>Results for Waterfalls</h2>
     <c:forEach items="${waterFallInfo}" var="waterfall">
+        <c:set var="photo" value="${photo}" scope="session"/>
         <table border = "1" width = "100%">
             <tr>
                 <th>Waterfall Information: </th>
             </tr>
             <tr>
                 <td>Waterfall Name: ${waterfall.name}<br />
-                    city: ${waterfall.city} <c:if test="${empty waterfall.city}">N/A</c:if><br />
-                    state: ${waterfall.state} <c:if test="${empty waterfall.state}">N/A</c:if> <br />
                     Latitude: ${waterfall.latitude}<c:if test="${empty waterfall.latitude}">N/A</c:if><br />
                     Longitude: ${waterfall.longitude}<c:if test="${empty waterfall.longitude}">N/A</c:if><br />
                     URL: ${waterfall.url}<c:if test="${empty waterfall.url}">N/A</c:if><br />
+                    Photo: ${photo.sourceUrl}<br />
                 </td>
                 <br />
             </tr>
