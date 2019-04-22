6fb52954e80f25142bf01ff128ece44db082302e
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-22 03:32:10 -0500

    Remove one insert statment to fix tests

--- src/test/resources/sql/photoSetup.sql
+++ src/test/resources/sql/photoSetup.sql
@@ -2,4 +2,3 @@ DELETE FROM photo;
 
 INSERT INTO photo (photo_id, waterfall_id, sourceURL) VALUES (1, 3, 'https://upload.wikimedia.org/wikipedia/commons/3/3a/GrandfatherExposedPreCambrian.jpg');
 INSERT INTO photo (photo_id, waterfall_id, sourceURL) VALUES (2, 1, 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Kalandula_waterfalls_of_the_Lucala-River_in_Malange%2C_Angola_%282%29.JPG/1200px-Kalandula_waterfalls_of_the_Lucala-River_in_Malange%2C_Angola_%282%29.JPG');
-INSERT INTO photo (photo_id, waterfall_id, sourceURL) VALUES (10, 1, 'https://upload.wikimedia.org/wikipedia/commons/thumb/...');
