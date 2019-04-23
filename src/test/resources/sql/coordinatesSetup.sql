fcffcbde2d45d02ab647492c38ef5e2a6f2a4bba
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-22 23:03:51 -0500

    Add coordinates test setup sql file

---
+++ src/test/resources/sql/coordinatesSetup.sql
@@ -0,0 +1,23 @@
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00601, 18.180555, -66.749961);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00602, 18.361945, -67.175597);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00603, 18.455183, -67.119887);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00606, 18.158345, -66.932911);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00610, 18.295366, -67.125135);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00612, 18.402253, -66.711397);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00616, 18.420412, -66.671979);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00617, 18.445147, -66.559696);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00622, 17.991245, -67.153993);
+INSERT INTO zip_coords (zipcode, latitude, longitude) VALUES (00623, 18.083361, -67.153897);
+
+
+
+
+
+
+
+
+
+
+
+
+
